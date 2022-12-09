4fa96b74f79cf71ee3ecb8e89d1702ca6eeba46e
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-12-09 15:52:43 +0100

    setup complete screen

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script></script>
 
 <template>
-  <div>
+  <div class="container">
     <p class="complete">Setup Complete</p>
   </div>
 </template>
 
 <style scoped lang="scss">
-p.complete {
-  color: green;
+div.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+
+  p.complete {
+    color: green;
+    font-size: 4rem;
+    font-weight: 600;
+  }
 }
 </style>
